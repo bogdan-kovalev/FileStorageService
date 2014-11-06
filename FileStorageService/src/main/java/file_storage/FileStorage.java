97b9fadb3efdcafd1c1b73e31140801226872752
Bohdan Kovalov <mstbit@i.ua>
2014-11-06 19:35:01 +0200

    api

--- FileStorageService/src/main/java/file_storage/FileStorage.java
+++ FileStorageService/src/main/java/file_storage/FileStorage.java
@@ -9,6 +9,8 @@ public interface FileStorage {
     void saveFile(String key, InputStream inputStream);
     void saveFile(String key, InputStream inputStream, long liveTimeMillis);
     InputStream readFile(String key);
+
+    void deleteFile(String key);
     double getFreeStorageSpace();
     void purge(double percents);
 }
